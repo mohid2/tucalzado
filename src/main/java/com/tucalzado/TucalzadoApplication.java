2bf77392ebeeb9e68118cadbcdcf854c5b27125c
mohid <fukahamaroc@gmail.com>
2024-06-02 16:23:20 +0200

    start project

---
+++ src/main/java/com/tucalzado/TucalzadoApplication.java
@@ -0,0 +1,13 @@
+package com.tucalzado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TucalzadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TucalzadoApplication.class, args);
+	}
+
+}
