1a10c0fe45b6e8f311b45a7f4c16265ab27b3275
mohid <fukahamaroc@gmail.com>
2024-06-06 17:57:10 +0200

    fix bugs on pom.xml

--- src/main/java/com/tucalzado/TucalzadoApplication.java
+++ src/main/java/com/tucalzado/TucalzadoApplication.java
@@ -1,9 +1,7 @@
 package com.tucalzado;
 
-import com.tucalzado.models.dto.ShoeDTO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class TucalzadoApplication {
